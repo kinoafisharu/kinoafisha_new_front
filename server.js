e217ec5ff71de202d2f629b663492a7bde2aed7b
Kolya Grebnev <mumiyasss@gmail.com>
2020-01-24 05:55:06 +0300

    express connect history api fallback

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 
+let history = require('connect-history-api-fallback');
+
 let app = express();
+app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
