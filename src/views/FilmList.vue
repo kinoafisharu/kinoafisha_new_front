98a0fa5f813155a885bbf91dfc06c5e41fe2305d
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-18 15:13:32 +0300

    Update FilmList.vue

--- src/views/FilmList.vue
+++ src/views/FilmList.vue
@@ -5,13 +5,14 @@
           <li v-for="item in filmList" :key="item.id">
             {{ item.id }}
             {{ item.year }}
-            {{ item.}}
+            {{ item.kid }}
+            {{ item.imdb_id}}
           </li>
         </ul>
     </div>
 </template>
 
-<script lang = 'ts'>
+<script lang='ts'>
 import { Vue } from "vue-property-decorator"
 import Axios, { AxiosResponse } from "axios"
 import { getListOfFilms } from "@/api/film"
@@ -21,6 +22,7 @@ import { FilmList } from "@/film-model"
 export default class FilmList extends Vue {
     private filmList!: FilmList
     private dataReady = false
+    
     async mounted() {
         this.filmList = await getListOfFilms()
         this.dataReady = true
