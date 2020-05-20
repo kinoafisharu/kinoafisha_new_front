2472cf55fd390806056f45aadc67c7c8138e3742
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-20 17:35:15 +0300

    Update FilmListSingleItem.vue

--- src/components/FilmListSingleItem.vue
+++ src/components/FilmListSingleItem.vue
@@ -5,7 +5,8 @@
     <p>====| {{film.year}} |====</p>
     <a :href="'{{url('ttps://www.imdb.com/title/tt')}}' + film.imdb_id">Go to imdb</a> 
     <p> ----------------------------------------- </p>
-    <a :href="'{{url('http://kinoinfo.ru/film')}}/' + film.kid">Go to kinoinfo</a> 
+    <a :href="'{{url('http://kinoinfo.ru/film')}}/' + film.kid">Go to kinoinfo</a>
+    <p> ----------------------------------------- </p>
   </li>
 
 </template>
