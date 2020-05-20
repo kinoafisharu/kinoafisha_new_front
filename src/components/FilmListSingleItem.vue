291b7df669561a0835ff7c1dc6c4287b78a81213
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-20 17:30:38 +0300

    Update FilmListSingleItem.vue

--- src/components/FilmListSingleItem.vue
+++ src/components/FilmListSingleItem.vue
@@ -4,8 +4,8 @@
     <router-link :to="{ name: 'filmdetail', params: { id:film.id }}">{{film.id}}</router-link>
     <p>====| {{film.year}} |====</p>
     <a href="https://www.imdb.com/title/tt{{film.imdb_id}}">Go to imdb</a>
+    <p> ----------------------------------------- </p>
     <a href="http://kinoinfo.ru/film/{{film.kid}}">Go to kinoinfo</a>
-    <hr>
   </li>
 
 </template>
