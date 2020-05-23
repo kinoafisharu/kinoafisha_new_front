9e9c31f5f9a33f3ed87b43203950ef6edcd3dc98
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-23 14:28:00 +0300

    Add files via upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
+  <div class = 'container'>
     <div class="home">
+      <router-link :to="{ name: 'films', params: {} }">Films</router-link>
     </div>
+  </div>
 </template>
 
 <script>
@@ -8,3 +11,18 @@ export default {
     name: "home"
 }
 </script>
+
+<style>
+.container {
+  margin: 300px;
+
+}
+.home {
+  width: 200px;
+  height: 100px;
+  padding-top: 50px;
+  position: flex;
+  text-align: center;
+
+}
+</style>
