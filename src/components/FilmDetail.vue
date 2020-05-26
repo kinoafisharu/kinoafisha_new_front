558ed85e54e801d7eb48c1111ae3668452aeaba1
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-26 18:52:16 +0300

    Update FilmDetail.vue

--- src/components/FilmDetail.vue
+++ src/components/FilmDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class = 'poster-and-info-block'>
-          <FilmPoster v-if = "film !== null" :kid = 'film.kid' :rate = 'film.imdb_rate' :agelimits = '14'/>
+          <FilmPoster v-if = "film !== null" :kid = 'film.kid' :rate = 'film.imdb_rate' :agelimits = '14' :filmlikes = 'film.likes'/>
           <p v-else-if = 'loading === true'>loading...</p>
           <p v-if = 'errored === true'> ERROR occured in FilmDetail component </p>
           <FilmInfo/>
