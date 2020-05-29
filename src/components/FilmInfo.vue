a39d0a2e8243cd8595879b3e24a89619d8ec7d60
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2020-05-29 19:01:30 +0300

    Переставил местами элементы

--- src/components/FilmInfo.vue
+++ src/components/FilmInfo.vue
@@ -1,18 +1,18 @@
 <template>
   <div class = 'filminfo'>
     <h1 v-if = 'title' id = 'title'>{{title}}</h1>
-    <h1 v-else> This film doesnt have a title yet </h1>
-    <h2 v-if = 'year' id = 'year'>{{year}}</h2>
+    <h1 v-else> Без названия </h1>
+        <div v-for = 'country in country' :key = 'country.name'>
+          <p id = 'description'>{{country.name}}, {{year}}</p>
+        </div>
     <div class = 'genre-block'>
-      <p id = 'description' v-if = 'genre[1]'>{{genre[1].name}} </p>
-      <p id = 'description' v-if = 'genre[0]'>{{genre[0].name}} </p>
+      <p id = 'description' v-if = 'genre[0]'>{{genre[0].name}} '/'
+      <p id = 'description' v-if = 'genre[1]'>{{genre[1].name}} '/'
       <p id = 'description' v-if = 'genre[2]'>{{genre[2].name}} </p>
     </div>
-        <div v-for = 'country in country' :key = 'country.name'>
-          <p id = 'description'>{{country.name}}, {{country.name_en}}</p>
-        </div>
+        
     <p v-if = 'description' id = 'description'>{{description}}</p>
-    <p v-else id = 'description'>This films doesn't have description yet, you can propose your version of description for this film by clicking the button below </p>
+    <p v-else id = 'description'> Аннотации пока нет. </p>
   </div>
 </template>
 
