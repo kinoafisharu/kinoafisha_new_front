6e6c18460a2e337cc77b159f05d8b29f196899f2
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2020-05-29 19:11:21 +0300

    перенес жанры между страной и годом

--- src/components/FilmInfo.vue
+++ src/components/FilmInfo.vue
@@ -3,14 +3,13 @@
     <h1 v-if = 'title' id = 'title'>{{title}}</h1>
     <h1 v-else> Без названия </h1>
         <div v-for = 'country in country' :key = 'country.name'>
-          <p id = 'description'>{{country.name}}, {{year}}</p>
+          <p id = 'description'>{{country.name}},
+          id = 'description' v-if = 'genre[0]'>{{genre[0].name}} '/'
+          id = 'description' v-if = 'genre[1]'>{{genre[1].name}} '/'
+          id = 'description' v-if = 'genre[2]'>{{genre[2].name}}, 
+          {{year}}</p>
         </div>
-    <div class = 'genre-block'>
-      <p id = 'description' v-if = 'genre[0]'>{{genre[0].name}} '/'
-      <p id = 'description' v-if = 'genre[1]'>{{genre[1].name}} '/'
-      <p id = 'description' v-if = 'genre[2]'>{{genre[2].name}} </p>
-    </div>
-        
+          
     <p v-if = 'description' id = 'description'>{{description}}</p>
     <p v-else id = 'description'> Аннотации пока нет. </p>
   </div>
