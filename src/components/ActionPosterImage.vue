fc9800a8982f50fb49e57bcc611b3ee37815975f
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-30 16:59:40 +0300

    Fixed rating

--- src/components/ActionPosterImage.vue
+++ src/components/ActionPosterImage.vue
@@ -176,7 +176,7 @@ export default {
     },
     ratecalced: function() {
       let r = this.rate
-      let rc = Math.round(r/2)
+      let rc = Math.round(r/2 + 1)
       if (rc > 5) rc = 5; else if (rc == 1) rc = 2;
       if (r == 0) return r; else return rc;
     },
