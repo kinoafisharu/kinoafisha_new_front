2c5b4d6e0b2cfa3a6ded45bc5f308ee9238ee386
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-06 14:42:38 +0300

    Кнопка стрелка

---
+++ src/components/global/buttons/ArrowButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class = 'icon-arrow'>
+    <img class = 'buttonimg' :src = "`${publicPath}images/arrow.png`">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'arrow-buttton',
+  data () {
+  return {
+    publicPath: process.env.BASE_URL
+  }
+}
+}
+</script>
+
+<style scoped lang='scss'>
+.buttonimg {
+  max-width: 100%;
+  max-height: 100%;
+  opacity: 0.1;
+}
+.buttonimg:hover {
+  opacity: 0.7;
+}
+</style>
