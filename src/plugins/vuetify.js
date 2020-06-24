26af141acc8799b0ae4e718839627399d88536cf
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-24 12:05:41 +0300

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,18 @@
+
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+//Плагин Vuetify
 
 Vue.use(Vuetify)
 
 const opts = {
   icons: {
     iconfont: 'mdiSvg',
-  }
+  },
+  theme: {
+    dark: true,
+  },
 }
 
 export default new Vuetify(opts)
